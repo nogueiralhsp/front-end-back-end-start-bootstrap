57798a00b4b58c2467ce207c0d25c34f52f460a6
Henrique Nogueira <henriquenogueira@henriques-mbp.home>
2021-09-15 16:56:34 +0100

    Content component update

--- client/src/components/Content.js
+++ client/src/components/Content.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="content">
       <header className="content-header">
-        <h1>this is the body of my footstrap</h1>
+        <h1>this is the body of my bootstrap</h1>
       </header>
     </div>
   )
